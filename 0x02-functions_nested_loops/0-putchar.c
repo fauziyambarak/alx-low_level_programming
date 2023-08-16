f83de0d80627a00b330082cdcdb8448711d7b1a0
fauziyambarak <fauziyambarak@gmail.com>
2023-08-16 05:41:12 -0700

    printing putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void _putchar() {
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+}
+
+int main() {
+    _putchar();
+    putchar('\n');
+    return 0;
+}
+
